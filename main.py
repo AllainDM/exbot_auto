9ed9225cd0bd4aa71719d7fe08f87e4be6c1a5c5
allain <allaindemoupu@mail.ru>
2024-04-16 07:36:28 +0300

    Обновлен список поселков сразу для двух файлов

--- main.py
+++ main.py
@@ -1719,6 +1719,7 @@ def save_from_goodscat_for_day(table, status, date2, area, t_o):
                 address[0] == " Репино" or \
                 address[0] == " Сестрорецк" or \
                 address[0] == "Янино-1" or \
+                address[0] == "Яни" or \
                 address[0] == " Песочный" or \
                 address[0] == " Лисий" or \
                 address[0] == " Горелово" or \
